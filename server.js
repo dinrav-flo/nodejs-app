c4f752fa3a4909d31e7269d3f13525cc1c7777e2
Dinesh <dinrav-flo@gmail.com>
2024-08-19 22:07:41 -0400

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get('/' , (req , res)=>{
-    res.send("server is listening on port 4000")
+    res.send("server is listening on port 4000.")
 })
 
 app.listen(4000 , ()=>{console.log("server listening on port 4000")});
